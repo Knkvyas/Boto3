1e148498dea3d0459b4e19225bbb22a6e8fd0c84
Kanak Vyas <kvyas0970@gmail.com>
2020-03-15 16:35:00 +0530

    Updated Bucket.py

--- Bucket.py
+++ Bucket.py
@@ -2,9 +2,11 @@ import boto3
 import botocore, botostubs
 import configparser
 
-import boto3
-
 s3 = boto3.resource('s3')
 location = {'LocationConstraint': 'ap-southeast-1'}
-s3.create_bucket(Bucket='kanak-bucket1', CreateBucketConfiguration=location)
-s3.Object('kanak-bucket1', 'abcd.txt').upload_file(Filename='/home/kanak_vyas/Documents/kanak-hu2020/Boto3/abcd.txt')
+s3.create_bucket(Bucket='Name-Bucket', CreateBucketConfiguration=location)
+s3.Object('Name-Bucket', 'abcd.txt').upload_file(Filename='path/abcd.txt')
+
+
+#path is the location of your file.
+#abcd.txt is file you want to uplaod
